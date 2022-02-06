b24295e4191030d1aa4e050e686c8204f12142d6
김민성 <sohoung4@gmail.com>
2022-02-06 22:47:01 +0900

    11653

---
+++ 09_기본수학2/김민성/Soinsu_11653.java
@@ -0,0 +1,26 @@
+package Baekjoon0105;
+
+import java.util.Scanner;
+
+public class Soinsu_11653 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+        if(number==1) {
+        }else {
+            for(int i=2; i<10000000; i++) {
+                for(int j=2; j<10000000; j++) {
+                    if(number%j ==0) {
+                        number=number/j;
+                        System.out.println(j);
+                        break;
+                    }
+                }
+                if(number==1) {
+                    break;
+                }
+            }
+        }
+    }
+}
