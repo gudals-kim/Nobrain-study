463e53889b56e20b9e6077ff73137abfff7479f7
김민성 <sohoung4@gmail.com>
2022-02-06 22:44:33 +0900

    11654

---
+++ 07_문자열/김민성/ASCII_11654.java
@@ -0,0 +1,14 @@
+package Baekjoon;
+
+
+import java.util.Scanner;
+
+public class ASCII_11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+
+        int i = (int)ch;
+        System.out.println(i);
+    }
+}
