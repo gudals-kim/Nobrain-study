76f74ab1f68b48282f86a6825eb16f4261948a27
김민성 <sohoung4@gmail.com>
2022-02-06 22:41:36 +0900

    1330

---
+++ 02_if문/김민성/Comparison_1330.java
@@ -0,0 +1,21 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Comparison_1330 {
+    public static void main(String[] args) {
+        int a,b;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        if(a < b) {
+            System.out.println("<");
+        }else if (a == b) {
+            System.out.println("==");
+        }else {
+            System.out.println(">");
+        }
+
+    }
+}
