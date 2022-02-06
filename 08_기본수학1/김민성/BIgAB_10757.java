7ccc06f27a02568886db92e89336138b74abd789
김민성 <sohoung4@gmail.com>
2022-02-06 22:45:43 +0900

    10757

---
+++ 08_기본수학1/김민성/BIgAB_10757.java
@@ -0,0 +1,20 @@
+package Baekjoon0105;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class BIgAB_10757 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+        BigInteger sum = A.add(B);
+
+        System.out.println(sum);
+    }
+}
